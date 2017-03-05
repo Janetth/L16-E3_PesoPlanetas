4f3bd0fe87b0fdc8674a953c2d56b485679ad056
Janetth <janetthvisa@gmail.com>
2017-03-05 17:09:54 -0500

    Modificando Función CalcularForm

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,14 +11,14 @@ function calcularForm(){
   		var pesoPlaneta = a * b;
   		return pesoPlaneta.toFixed(2);
 	 }
-   alert("\n Tu peso en otros mundos del Sistema solar :"
-           + "\n Tu peso en Mercurio es :" + calPeso(masa,3.7)+ " Kg"
-           + "\n Tu peso en Venus es :" + calPeso(masa,8.87)+ "Kg"
-           + "\n Tu peso en Marte es :" + calPeso(masa,3.71)+ "Kg"
-           + "\n Tu peso en Jupiter es :" + calPeso(masa,23.12)+ " Kg"
-           + "\n Tu peso en Saturno es :" + calPeso(masa,8.69)+ "Kg"
-           + "\n Tu peso en Urano es :" + calPeso(masa,8.69)+ "Kg"
-           + "\n Tu peso en Neptuno es :" calPeso(masa,11)+ "Kg");
+   alert("\n Tu peso en otros mundos del Sistema solar :\n"
+           + "\n Tu peso en Mercurio es : " + calPeso(masa,3.7)+ " Kg."
+           + "\n Tu peso en Venus es : " + calPeso(masa,8.87)+ " Kg."
+           + "\n Tu peso en Marte es : " + calPeso(masa,3.71)+ " Kg."
+           + "\n Tu peso en Jupiter es : " + calPeso(masa,23.12)+ " Kg."
+           + "\n Tu peso en Saturno es : " + calPeso(masa,8.69)+ " Kg."
+           + "\n Tu peso en Urano es : " + calPeso(masa,8.69)+ " Kg."
+           + "\n Tu peso en Neptuno es : " + calPeso(masa,11)+ " Kg.");
    form1.reset();
    return false;
  }
