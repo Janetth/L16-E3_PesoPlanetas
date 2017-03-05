c5d21bb2fef855f06900a269068a4f965e245645
LeidyRosmery <rosm3rita@hotmail.com>
2017-03-05 15:25:39 -0500

    commit ross

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,20 @@ element.addEventListener("submit", function(event) {
 });
 function calcularForm(){
 
-   var form1 = document.getElementsByName("form1")[0];
+  var form1 = document.getElementsByName("form1")[0];
+  var peso= document.getElementById("peso");
+  var jp = 0;
+  jp = (10 * peso.value * 2.533 ) / 10;
 
-   /* Escribe tú código aquí */
+  var ur =0;
+  ur =(10 * peso.value * 0.905 ) / 10;
 
+  var pesMart = 0;
+  pesMart = (10 * peso.value * .379 ) / 10;
 
-
-   alert ("Tu peso en otros planetas es:.......");
+ alert("\n Tu peso en Jupiter es :" + jp.toFixed(2)+ " Kg"
+  + " \n Tu peso en Urano es :" + ur.toFixed(2)+ "Kg" +
+  "\n Tu peso en Marte es :" + pesMart.toFixed(2));
    form1.reset();
    return false;
-}
+ }
