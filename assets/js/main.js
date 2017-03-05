b012b6fdddcf4b700c7b22cda8376e8cf507224e
YesseniaHa <yesel1601@hotmail.com>
2017-03-05 13:04:56 -0500

    First commit Yess

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,24 @@ element.addEventListener("submit", function(event) {
 function calcularForm(){
 
    var form1 = document.getElementsByName("form1")[0];
-
-   /* Escribe tú código aquí */
-
-
-
-   alert ("Tu peso en otros planetas es:.......");
+   var masa = parseFloat(document.getElementById('peso').value)/9.8;
+    function calculoMercurio(){
+		var mercurio = masa * 3.7;
+		return mercurio.toFixed(2);
+	}
+	function calculoJupiter(){
+		var jupiter = masa * 24.79;
+		return jupiter.toFixed(2);
+	}
+	function calculoVenus(){
+		var venus = masa * 8.87;
+		return venus.toFixed(2);
+	}
+	
+  
+   alert ("Tu peso en Mercurio es: " + calculoMercurio() );
+   alert ("Tu peso en Jupiter es: " + calculoJupiter() );
+   alert ("Tu peso en Venus es: " + calculoVenus() );
    form1.reset();
    return false;
 }
