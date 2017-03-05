53bc0bc05305f4860466aae5d2f0833db1d13653
Janetth <janetthvisa@gmail.com>
2017-03-05 17:37:04 -0500

    Último commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,26 +5,26 @@ var element = document.querySelector(".form-signup");
 });
 function calcularForm(){
    var form1 = document.getElementsByName("form1")[0];
-   var masa = .value;
+   var masa = document.getElementById('peso').value;
    var masa =parseFloat(masa)/9.8;
    if(isNaN(masa)){
      alert("Ingrese un valor numérico");
-     document.getElementById('peso')
-
+     document.getElementById('peso').value="";
+     document.getElementById('peso').focus();
      return false;
    }
    function calPeso(a,b){
   		var pesoPlaneta = a * b;
   		return pesoPlaneta.toFixed(2);
 	 }
-   alert("\n Tu peso en otros mundos del Sistema Solar :\n"
+   alert("\n Tu peso en otros mundos del Sistema Solar:\n"
            + "\n Tu peso en Mercurio es : " + calPeso(masa,3.7)+ " Kg."
            + "\n Tu peso en Venus es : " + calPeso(masa,8.87)+ " Kg."
            + "\n Tu peso en Marte es : " + calPeso(masa,3.71)+ " Kg."
            + "\n Tu peso en Jupiter es : " + calPeso(masa,23.12)+ " Kg."
            + "\n Tu peso en Saturno es : " + calPeso(masa,8.69)+ " Kg."
            + "\n Tu peso en Urano es : " + calPeso(masa,8.69)+ " Kg."
-           + "\n Tu peso en Neptuno es : " + calPeso(masa,11)+ " Kg.");
+           + "\n Tu peso en Neptuno es : " + calPeso(masa,11)+ " Kg.\n");
    form1.reset();
    return false;
  }
